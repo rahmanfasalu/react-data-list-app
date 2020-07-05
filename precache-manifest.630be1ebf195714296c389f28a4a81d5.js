0aff9a3114d5848d160d7f6b5207946a34aeafc1
Fasalu Rahman <fasalukm@gmail.com>
2020-07-05 09:53:45 +0200

    Deploy

---
+++ precache-manifest.630be1ebf195714296c389f28a4a81d5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1c72377d214dcf7121308a242c610e",
+    "url": "/react-data-list-app/index.html"
+  },
+  {
+    "revision": "3fd9f86f7401a226533f",
+    "url": "/react-data-list-app/static/css/2.8ebc772a.chunk.css"
+  },
+  {
+    "revision": "3fd9f86f7401a226533f",
+    "url": "/react-data-list-app/static/js/2.e5c3886a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-data-list-app/static/js/2.e5c3886a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caec23e44d33c7e96fee",
+    "url": "/react-data-list-app/static/js/main.32536ed3.chunk.js"
+  },
+  {
+    "revision": "9f50fa0b223e636b0380",
+    "url": "/react-data-list-app/static/js/runtime-main.f7479d1b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-data-list-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-data-list-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-data-list-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-data-list-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-data-list-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "165a59db038ed62e1aff07dc0e62ae55",
+    "url": "/react-data-list-app/static/media/logo.165a59db.svg"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/react-data-list-app/static/media/open-sans-v17-latin-regular.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/react-data-list-app/static/media/open-sans-v17-latin-regular.de0869e3.woff"
+  }
+]);
